473a72dcfe3435e415c4ff6e32612705de9d2de4
Pavel <paveliva1988@gmail.com>
2024-01-13 17:48:40 +0300

    add footer element

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import "../../index.css";
+import Main from '../main/Main';
+import Footer from '../footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Main>
+      </Main>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+      <footer className="footer">
+        <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+        <div className="footer__line"></div>
+        <div className="footer__signature-wrapper"> 
+          <p className="footer__signature-caption">© 2020</p>
+          <ul className="footer__signature-label">
+            <li className="footer__signature-label_item">Яндекс.Практикум</li>
+            <li className="footer__signature-label_item">Github</li>
+          </ul>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
